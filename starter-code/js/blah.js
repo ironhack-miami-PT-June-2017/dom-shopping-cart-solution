a1633e9e1071722025dbe7a84929d13b93c27a73
Nicholas Borbe <nicholasborbe@Nicholass-MacBook-Pro-2.local>
2017-07-10 19:10:33 -0400

    removed regex and function scope event listeners for readability

--- starter-code/js/blah.js
+++ starter-code/js/blah.js
@@ -5,17 +5,20 @@ var getTotalProductPrice = document.getElementsByClassName('totalProductPrice');
 var deleteButtons = document.getElementsByClassName('btn-delete');
 
 //Click event for delete buttons
-function deleteRow(){
+// function deleteRow(){
   for(i = 0; i < getProduct.length; i++) {
     deleteButtons[i].addEventListener('click', function (e) {
       e.currentTarget.parentNode.parentNode.remove();
     });
   }
-}
+// }
+
+
+// replace(/[^0-9\.]+/g,""))
 //Calculate prices for individual products
   function calculatePrices() {
     for( i = 0; i < getProduct.length; i++ ) {
-      var totalPrice = Number(getUnitPrice[i].innerHTML.replace(/[^0-9\.]+/g,"")) * getUnitQty[i].value;
+      var totalPrice = Number(getUnitPrice[i].innerHTML * getUnitQty[i].value);
       document.getElementsByClassName('totalProductPrice')[i].innerHTML = totalPrice;
     }
     getTotalPrice();
@@ -45,7 +48,7 @@ var getValueofProductCost = document.getElementById('productCostValue').value;
 var getCreateBtn = document.getElementById('createBtn');
 var getCreateDiv = document.getElementById('create');
 
-function createRow() {
+// function createRow() {
   getCreateBtn.addEventListener('click', function (e) {
     var getValueofProductName = document.getElementById('productNameValue').value;
     var getValueofProductCost = document.getElementById('productCostValue').value;
@@ -54,10 +57,11 @@ function createRow() {
     newProductRow.innerHTML = newProductHtml()
     newProductRow.querySelector('.productName').innerHTML = getValueofProductName;
     newProductRow.querySelector('.productCost').innerHTML = getValueofProductCost;
-    document.getElementsByClassName('container')[0].insertBefore(newProductRow, getCreateDiv.parentNode);
+    document.getElementsByClassName('container')[0].appendChild(newProductRow);
     //deleteRow();
   });
-}
+  // , getCreateDiv.parentNode
+// }
 
 function newProductHtml(){
   return '<div class="col-xs-5"> <span class="productName"> </span> </div> <div class="col-xs-5">' +
@@ -73,7 +77,7 @@ function newProductHtml(){
   '<div class="col-xs-5 delete">' +
     '<button class="btn btn-delete">DELETE</button></div>';
 }
-createRow();
-deleteRow();
+// createRow();
+// deleteRow();
 
 //done
